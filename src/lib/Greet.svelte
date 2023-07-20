45f5fc99b2c6caa41ff06698dab9f5f196bfb561
Yehra <guillaumegirardpro@gmail.com>
2023-07-20 08:51:20 +0200

    first commit

---
+++ src/lib/Greet.svelte
@@ -0,0 +1,27 @@
+<script>
+	import { invoke } from '@tauri-apps/api/tauri';
+
+	let name = '';
+	let greetMsg = '';
+
+	async function greet() {
+		greetMsg = await invoke('greet', { name });
+	}
+</script>
+
+<div>
+	<input id="greet-input" placeholder="Enter a name..." bind:value={name} />
+	<button on:click={greet}>Greet</button>
+	<p>{greetMsg}</p>
+</div>
+
+<style lang="postcss">
+	button {
+		@apply font-bold py-2 px-6 rounded;
+		@apply bg-blue-500 text-white;
+	}
+
+	button:hover {
+		@apply bg-blue-700;
+	}
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Greet from '../lib/Greet.svelte';
+</script>
+
+<h1>Welcome to SvelteKit</h1>
+<Greet />
+
+<style lang="postcss">
+	h1 {
+		@apply text-4xl font-bold;
+	}
+</style>
